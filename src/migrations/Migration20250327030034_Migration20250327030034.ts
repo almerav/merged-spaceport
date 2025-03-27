06c037c7c06874a471a791ebc37e04f902562b06
Richie Paul Aquiño <richieaquino56@gmail.com>
2025-03-27 11:32:13 +0800

    chore: add migration for users

--- src/migrations/Migration20250327030034_Migration20250327030034.ts
+++ src/migrations/Migration20250327030034_Migration20250327030034.ts
@@ -2,10 +2,24 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20250327030034_Migration20250327030034 extends Migration {
   override async up(): Promise<void> {
-    this.addSql(`select 1`);
+    this.addSql(`
+      ALTER TABLE "users"
+      DROP COLUMN IF EXISTS "deleted_at",
+      DROP COLUMN IF EXISTS "is_deleted",
+      RENAME COLUMN "first_name" TO "firstname",
+      RENAME COLUMN "last_name" TO "lastname",
+      RENAME COLUMN "created_at" TO "createdat";
+      `);
   }
 
   override async down(): Promise<void> {
-    this.addSql(`select 1`);
+    this.addSql(`
+      ALTER TABLE "users"
+      ADD COLUMN "deleted_at" TIMESTAMP NULL,
+      ADD COLUMN "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+      RENAME COLUMN "firstname" TO "first_name",
+      RENAME COLUMN "lastname" TO "last_name",
+      RENAME COLUMN "createdat" TO "created_at";
+    `);
   }
 }
